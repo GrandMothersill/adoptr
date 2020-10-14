7b480fd0ba8ebb19e815693fe1c7ae602503adec
Erica Sun <ericasun@seas.upenn.edu>
2020-10-14 16:13:27 -0400

    use material ui for user login

--- adoptr-frontend/src/App.js
+++ adoptr-frontend/src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import UserRegistration from "./components/userRegistration.js";
 import AnimalRegistration from "./components/animalRegistration.js";
 import ShelterRegistration from "./components/shelterRegistration.js";
-import Login from "./components/Login.js";
 import ShelterLogin from "./components/ShelterLogin.js";
 import Nav from "./components/Nav";
 import About from "./components/About"
 import AnimalProfile from "./components/AnimalProfile"
+import UserLogin from "./components/UserLogin"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import useApplicationData from "./hooks/useApplicationData";
+
+
 
 function App() {
 
@@ -28,9 +29,11 @@ function App() {
           <Route path='/animals' exact component={AnimalRegistration} />
           <Route path='/users' render={() => <UserRegistration login={setUser} state={state} />} />
           <Route path='/shelters' render={() => <ShelterRegistration login={setShelter} state={state} />} />
-          <Route path='/login' render={() => <Login login={setUser} state={state} />} />
+          <Route path='/login' render={() => <UserLogin login={setUser} />} />
+          {/* <Route path='/login' render={() => <Login login={setUser} state={state} />} /> */}
           <Route path='/shelterlogin' render={() => <ShelterLogin login={setShelter} state={state} />} />
           <Route path='/' exact component={About} />
+          
         </Switch>
       </div>
     </Router>
