373ec9c985465eab1bb12a7d3ad43049abd740c6
Erica Sun <ericasun@seas.upenn.edu>
2020-10-16 18:51:29 -0400

    separate dashboard component from landing

---
+++ adoptr-frontend/src/components/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function Dashboard(props) {
+  const [loading, setLoading] = useState(true);
+  const [profiles, setProfiles] = useState();
+  let profileCards = [];
+
+  useEffect(() => {
+    axios
+    .get(`http://localhost:3001/profiles/?name=${props.name}&id=${props.id}`) 
+    .then((response) => {
+      setProfiles(response.data);
+      setLoading(false);
+    })
+    .catch((err) => {
+      alert(err);
+    })
+  }, []);
+
+  if (!loading) {
+    profiles.forEach(profile => {
+      profileCards.push(
+      <Card style={{ width: '18rem' }} key={profile._id}>
+        <Card.Img variant="top" src={profile.animal_photos[0]} />
+        <Card.Body>
+          <Card.Title>{profile.name}</Card.Title>
+          <Card.Text>
+            {profile.bio}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      )
+    })
+  }
+  return profileCards;
+}
+
+
+export default Dashboard;
--- adoptr-frontend/src/components/ShelterLanding.js
+++ adoptr-frontend/src/components/ShelterLanding.js
@@ -4,58 +4,23 @@ import { Redirect, Link } from "react-router-dom"
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import "../styles/Landing.css";
+import Dashboard from "./Dashboard";
 
 function ShelterLanding(props) {
-  const [isLoading, setLoading] = useState(true);
-  const [profiles, setProfiles] = useState();
-  let profileCards = [];
+  // const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const id = props.state.account._id;
-    const name = props.state.account.name;
-    axios
-    .get(`http://localhost:3001/profiles/?name=${name}&id=${id}`) 
-    .then((response) => {
-      setProfiles(response.data);
-      setLoading(false);
-    })
-    .catch((err) => {
-      alert(err);
-    })
-  }, []);
- 
   if (!props.state.type) {
     return <Redirect to="/" />
   } else if (props.state.type === "user") {
     return <Redirect to="/landing" />
   }
 
-  if (isLoading) {
-    return (<div><p>Loading..</p></div>)
-  } else {
-    profiles.forEach(profile => {
-      profileCards.push(
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={profile.animal_photos[0]} />
-        <Card.Body>
-          <Card.Title>{profile.name}</Card.Title>
-          <Card.Text>
-            {profile.bio}
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      )
-    })
-  }
   return (
     <div className="landing">
       <h1>Welcome, {props.state.account.name}</h1>
       <Link to="/animals">Create New Animal Profile</Link>
       <Link to="/profiles">Profiles</Link>
-      <div className="row">
-        {profileCards}
-      </div>
+      <div className="row"><Dashboard name={props.state.account.name} id={props.state.account._id} /></div>
     </div>
   )
 }
