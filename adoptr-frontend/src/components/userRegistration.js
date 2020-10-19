e47c343a10cf4c20cd9721a003707fb986c9e4e9
Erica Sun <ericasun@seas.upenn.edu>
2020-10-19 15:15:10 -0400

    done styling register pages

--- adoptr-frontend/src/components/userRegistration.js
+++ adoptr-frontend/src/components/userRegistration.js
@@ -1,8 +1,39 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import { Redirect } from "react-router-dom"
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import animals from "../images/animals.png";
+import "../styles/Login.css"
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  
 function UserRegistration(props) {
+    const classes = useStyles();
     const [landingRedirect, setLandingRedirect] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -51,51 +82,88 @@ function UserRegistration(props) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h1>Create Account</h1>
-            <label>
-                Full Name
-                <input
-                    name="name"
-                    type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    required />
-            </label>
-            <br></br>
-            <label>
-                Email
-                <input
-                    name="email"
-                    type="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    required />
-            </label>
-            <br></br>
-            <label>
-                Password:
-                <input
-                    name="password"
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    required />
-            </label>
-            <br></br>
-            <label>
-                Display Picture:
-                <input
-                    name="userPhoto"
-                    type="userPhoto"
-                    value={userPhoto}
-                    onChange={e => setUserPhoto(e.target.value)}
-                    required />
-            </label>
-            <br></br>
-
-            <button>Submit</button>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <img className="animals" src={animals} alt="animals"/>
+        <Typography component="h1" variant="h5">
+          User Registration
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="name"
+                label="Full Name"
+                name="name"
+                autoComplete="name"
+                onChange={e => setName(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="email"
+                label="Email Address"
+                type="text"
+                id="email"
+                autoComplete="email"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="password"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="photo"
+                label="Profile Photo"
+                type="text"
+                id="password"
+                autoComplete="photo"
+                onChange={e => setUserPhoto(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            style={{"background-color": "black"}}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/login" variant="body2" style={{"color": "black"}}>
+                Already have an account? Login
+              </Link>
+            </Grid>
+          </Grid>
         </form>
+      </div>
+    </Container>
     );
 }
 
