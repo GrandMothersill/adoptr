2bf0a676aa7ad4c8a5114a61b118932528ff1094
Erica Sun <ericasun@seas.upenn.edu>
2020-10-20 18:08:57 -0400

    style buttons

--- adoptr-frontend/src/components/UserLanding.js
+++ adoptr-frontend/src/components/UserLanding.js
@@ -37,9 +37,9 @@ function UserLanding(props) {
         <h1>Welcome to your dashboard, {props.state.account.name}</h1>
         <p>Edit your account, view your matches and swipe through animal profiles all in one place!</p>
         <div className="dashboard-actions">
-          <Link to="/user/edit"><Button variant="warning">Edit Account</Button></Link>
-          <Link to="/usermatches"><Button variant="warning" className="action">View your matches</Button></Link>
-          <Button onClick={() => handleRejectionReset(props.state.account._id)} variant="warning" className="action">Bring Back Rejected Animals</Button>
+          <Link to="/user/edit"><Button variant="warning" className="dashboard-actions">Edit Account</Button></Link>
+          <Link to="/usermatches"><Button variant="warning" className="dashboard-actions action">View your matches</Button></Link>
+          <Button onClick={() => handleRejectionReset(props.state.account._id)} variant="warning" className="dashboard-actions action">Bring Back Rejected Animals</Button>
         </div>
         <TinderSwipe setUserMatches={props.setUserMatches} userMatches={props.userMatches} setNewMatch={props.setNewMatch} setRejectedAnimal={props.setRejectedAnimal} state={props.state} />
       </div>
